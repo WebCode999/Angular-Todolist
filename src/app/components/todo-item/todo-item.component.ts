7627883b4331943501d370f040b029f689087c39
Alejandrotop <top.tech.prog@gmail.com>
2019-10-21 00:53:08 -0500

    add header

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todo-item',
@@ -8,8 +9,9 @@ import { Todo } from 'src/app/models/Todo';
 })
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter();
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
   }
@@ -22,9 +24,13 @@ export class TodoItemComponent implements OnInit {
     return classes;
   }
   onToggle(todo) {
+    // Toggle in UI
     todo.completed = !todo.completed;
+    // Toggle on Server
+    this.todoService.toggleCompleted(todo).subscribe(todo =>
+      console.log(todo));
   }
   onDelete(todo) {
-    console.log('delete');
+    todo.deleteTodo.emit(todo);
   }
 }
